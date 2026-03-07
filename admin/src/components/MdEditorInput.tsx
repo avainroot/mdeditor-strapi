d09037d8b2c03069e7fc253092a7c58b4a3a86db
Ava <ava.inroot@gmail.com>
2026-03-07 13:59:19 +0300

    Field visual

--- admin/src/components/MdEditorInput.tsx
+++ admin/src/components/MdEditorInput.tsx
@@ -1,26 +1,50 @@
 import { MDEditor } from "@rootflow/mdeditor"
+import { Field } from "@strapi/design-system"
+import { useIntl } from "react-intl"
 
-interface Attribute {
+import type { MessageDescriptor } from 'react-intl'
+
+interface IntlObject extends MessageDescriptor {
+  defaultMessage: string
+}
+
+interface CustomFieldAttribute {
   type: string
+  customField: string
+  [key: string]: unknown
 }
 
 interface MdEditorInputProps {
-  attribute: Attribute
+  attribute: CustomFieldAttribute
   name: string
   value: string
-  onChange: (event: { target: { name: string; type: string; value: string } }) => void
+  onChange: (event: { target: { name: string; value: string; type: string } }) => void
+  intlLabel: IntlObject
+  description?: IntlObject
+  placeholder?: IntlObject
+  hint?: string
+  error?: IntlObject
+  contentTypeUID?: string
+  type?: string
+  required?: boolean
   disabled?: boolean
 }
 
-const MdEditorInput = ({ attribute, name, value, onChange, disabled }: MdEditorInputProps) => {
+const MdEditorInput = ({ attribute, name, value, onChange, disabled, required, error, hint, intlLabel }: MdEditorInputProps) => {
+  const { formatMessage } = useIntl()
   return (
-    <MDEditor
-      value={value ?? ""}
-      onChange={(val: string) =>
-        onChange({ target: { name, type: attribute.type, value: val } })
-      }
-      disabled={disabled}
-    />
+    <Field.Root name={name} required={required} error={error} hint={hint}>
+      <Field.Label>{formatMessage(intlLabel)}</Field.Label>
+      <MDEditor
+        value={value ?? ""}
+        onChange={(val: string) =>
+          onChange({ target: { name, type: attribute.type, value: val } })
+        }
+        disabled={disabled}
+      />
+      <Field.Hint />
+      <Field.Error />
+    </Field.Root>
   )
 }
 
